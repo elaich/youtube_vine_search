9a2a0486e59a8bb1f7bd2d759b296c9fa17416c2
El Aich Marouane <marouane@nuorder.io>
2015-06-26 01:33:13 +0000

    Youtube results

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 var express = require('express'); 
+var Youtube = require('youtube-api');
 var app = express();
 
+Youtube.authenticate({
+  type: "key",
+  key: "AIzaSyBgRwbBIjjisuA7zE0uNbhb1OuA7pGPuNM"
+})
+
+/*
 app.get('/', function(req, res) {
   res.end('Hello World!');
 })
+*/
+
+app.get('/api/search/youtube', function(req, res) {
+  youtubeSearch(
+    req.query.q, 
+    req.query.token, 
+    function(err, result) {
+      if (err) {
+        console.log(err);
+        return res.status(500).end();
+      }
+      res.json(result);
+  })
+})
+
+function youtubeSearch(q, token, callback) {
+  Youtube.search.list({
+    part: 'id,snippet', 
+    q: 'dogs', 
+    pageToken: '',
+    maxResults: 10
+  }, callback)
+}
 
 // listening
 var port = process.env.PORT || 9000;
